6bbe65a8b241da4688f1e8775eccabacfca87bdb
Dias, Leon <ldias@netsuite.com>
2024-11-21 00:04:26 -0300

    Grouping constants declaration

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-//import * as server from "./src/server";
-
 process.title = "MyWebServer";
-const args = process.argv;
-const port = args[2] || 7070,
+const args = process.argv,
+  port = args[2] || 7070,
   webServer = require("./src/server");
 
 webServer.server.listen(port, function () {
